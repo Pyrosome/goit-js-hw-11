0e247455a5910a6509eed59edba2cf0ee50c665f
Gaultheria <valeriaburina@gmail.com>
2023-01-21 13:49:23 +0100

    start task

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import Axios from 'axios';
-
-// <div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>;
